38fff6ee20b87a1cffd25bcd1fb59b03ebe7cfd0
iDentity <JaX__1988@hotmail.com>
2023-10-30 23:23:41 +0100

    Changes to AuraCharacterBase
    Created Character folder and added AuraCharacterBase to it.
    Removed default comments and Tick in AuraCharacterBase.

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright SeaStudios
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
